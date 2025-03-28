d5049838c394ab64306fef6cfaab2601ff3f9068
Vinicio <afteenoon175@gmail.com>
2025-03-28 16:28:25 -0600

    form-admin membresia

---
+++ src/main/java/com/example/hotelstaclara/controllers/UserControllers/USERhabitaciones.java
@@ -0,0 +1,49 @@
+package com.example.hotelstaclara.controllers.UserControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class USERhabitaciones {
+
+    @FXML
+    private Button btn_Clientes;
+
+    @FXML
+    private Button btn_Habitaciones;
+
+    @FXML
+    private Button btn_Pagos;
+
+    @FXML
+    private Button btn_Reservaciones;
+
+    @FXML
+    private Button but_editar;
+
+    @FXML
+    void btn_Clientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Habitaciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Pagos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Reservaciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void but_editar(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/hotelstaclara/controllers/UserControllers/USERreservaciones.java
@@ -0,0 +1,82 @@
+package com.example.hotelstaclara.controllers.UserControllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class USERreservaciones {
+
+    @FXML
+    private Button bnt_Buscar;
+
+    @FXML
+    private Button btn_Clientes;
+
+    @FXML
+    private Button btn_Eliminar;
+
+    @FXML
+    private Button btn_Habitaciones;
+
+    @FXML
+    private Button btn_Pagos;
+
+    @FXML
+    private Button btn_Reservaciones;
+
+    @FXML
+    private Button but_agregar;
+
+    @FXML
+    private Button but_editar;
+
+    @FXML
+    private TextField txt_Buscador;
+
+    @FXML
+    void btn_Buscar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Clientes(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Eliminar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Habitaciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Pagos(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_Reservaciones(ActionEvent event) {
+
+    }
+
+    @FXML
+    void but_agregar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void but_editar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txt_Buscar(ActionEvent event) {
+
+    }
+
+}
